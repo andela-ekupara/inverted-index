f1898d8f2ec49d787a75e4c396e7934bb5325b27
andela-ekupara <edwin.kupara@andela.com>
2016-02-02 18:15:22 +0300

    Added performance tests

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,47 +1,124 @@
-describe("Tests for InvertedIndex Checkpoint", function() {
+describe('Tests for InvertedIndex Checkpoint', function() {
 	var testIndex = new Index();
 	var bookData, index;
-	
+
 	beforeEach(function(done) {
 		// get data from books.json
-		testIndex.getBookData("books.json").done(function(data) {
+		testIndex.getBookData('books.json').done(function(data) {
 			bookData = data;
 			index = testIndex.createIndex(bookData);
 			done();
 		});
+		spyOn(testIndex, 'createIndex').and.callThrough();
   	});
 
-	describe("Read book data", function() {
-		it("The books.json file should not be empty", function() {
-			expect(typeof bookData).toBe("object");
+	describe('Read book data', function() {
+
+		it('The books.json file should not be empty', function() {
+			expect(bookData).toBeDefined();
+			expect(typeof bookData).toBe('object');
 			expect(bookData.length).not.toEqual(0);
 		});
 	});
-	describe("Populate Index", function() {
-		it("creates an index", function() {
+	describe('Populate Index', function() {
+		console.log();
+		it('creates an index', function() {
+			expect(index).toBeDefined();
 			expect(typeof index).toBe('object');
-			expect(Object.keys(index).length).not.toBe(0);
-			expect(index.hasOwnProperty('alice')).toBeTruthy();
-			expect(Array.isArray(index['alice'])).toBe(true);
+			var indexKeys = Object.keys(index);
+			expect(indexKeys.length).not.toBe(0);
+			for (var key in indexKeys){
+				expect(index.hasOwnProperty(indexKeys[key])).toBeTruthy();
+				expect(Array.isArray(index[indexKeys[key]])).toBe(true);
+			}
+
 		});
-		it("creates the correct index", function() {
-			//correct index
+
+		it('spy on the createIndex function', function(done) {
+
+			var thisIndex = new Index();
+			thisIndex.getBookData('books.json')
+				.done(function(data) {
+				bookData = data;
+				index = testIndex.createIndex(bookData);
+				done();
+			});
+			expect(testIndex.createIndex).toHaveBeenCalled();
+			expect(testIndex.createIndex).toHaveBeenCalledWith(bookData);
+
+		});
+
+		it('creates the correct index', function() {
+			// index is an object, no punctuations or uppercases
 			expect(typeof index).toBe('object');
-			expect(Array.isArray(index['dragon'])).toBeTruthy();
-			expect(index['dragon']).toEqual([2]);
-			expect(index['dwarf']).toEqual([0, 1, 2]);
+			expect(Array.isArray(index.dragon)).toBeTruthy();
+			expect(index.dragon).toEqual([2]);
+			expect(index.dwarf).toEqual([0, 1, 2]);
+			expect(index['.']).not.toBeDefined();
+			expect(index.Alice).not.toBeDefined();
 		});
 	});
-	describe("Search Index", function() {
-		it("returns indices for the provided search term", function() {
+	describe('Search Index', function() {
+
+		it('returns indices for the provided search term', function() {
 			//return indices for the search term
+			var results = testIndex.searchIndex('alice');
+			expect(typeof results).toBe('object');
+			expect(Array.isArray(results.alice)).toBeTruthy();
+			expect(results.alice).toEqual([0]);
+		});
+
+		it('can handle a string with more than one word', function () {
 			var results = testIndex.searchIndex('alice dwarf');
-			expect(results['alice']).toEqual([0]);
-			expect(results['dwarf']).toEqual([0, 1, 2]);
+			expect(results.alice).toEqual([0]);
+			expect(results.dwarf).toEqual([0, 1, 2]);
 			expect(results).toEqual({
 				'alice': [0],
 				'dwarf': [0, 1, 2]
-			})
+			});
 		});
+
+		it('can handle an array of strings', function () {
+			var results = testIndex.searchIndex(['book', 'dwarf', 'alliance']);
+			expect(results.book).toEqual([2]);
+			expect(results.dwarf).toEqual([0, 1, 2]);
+			expect(results.alliance).toEqual([1, 2]);
+			expect(results).toEqual({
+				'alliance': [1, 2],
+				'book': [2],
+				'dwarf': [0, 1, 2],
+			});
+		});
+
+		it('should return an error if term does not exist', function() {
+			var results = testIndex.searchIndex('haha');
+			expect(results.haha)
+				.toEqual('Error: the term couldn\'t be found');
+		});
+
+		it('does not take very long to search for a large array', function() {
+      		var terms = [
+				        'alice', 'rings', 'lord', 'wonderland',
+				        'enters', 'imagination', 'hole', 'rabbit',
+				        'world', 'elf', 'dwarf', 'hobbit', 'wizard',
+				        'destroy', 'ring', 'seek', 'alliance', 'man',
+				        'book'
+				      ];
+
+       		// Start tracking time just before the function is called
+		    var start = performance.now();
+
+
+		    var results = testIndex.searchIndex(terms);
+
+		    // Check the time after the function is done
+		    var end = performance.now();
+
+		    // check that the correct results are returned
+		    expect(Object.keys(results).length).toBe(terms.length);
+
+		      // check that the time is below 1 ms
+		    expect(end - start).toBeLessThan(1);
+    	});
 	});
 });
--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -1,20 +1,20 @@
-Index = function(filePath) {
+var Index = function() {
 
   this.index = {};
   this.data = [];
   //var self = this;
 
-  // fetch data from books.json   
+  // fetch data from books.json
   this.getBookData = function(filePath) {
     return $.getJSON(filePath);
-  }
+  };
 
   // takes data from the JSON file and creates an index from it
   this.createIndex = function(data) {
     var self = this;
     // create index and return it as an object
 
-    bookData = data;
+    var bookData = data;
 
     // remove punctuations
     var noPunct = removePunct(data);
@@ -28,20 +28,23 @@ Index = function(filePath) {
 
     var pattern = /[.',:]/gi;
 
-    // create object with noDups as keys and an array for each showing which document
-    // they key is found in
+    // create object with noDups as keys and an array for each showing
+    // which document the key is found in
     for (var i = 0; i < noDups.length; i++) {
 
         // initialize array as empty.
         self.index[noDups[i]] = [];
         var found = [];
-        // use JSON data with objects who index j will be pushed to the array found
+        // use JSON data with objects who index j will be
+        // pushed to the array found
         for (var j = 0; j < bookData.length; j++) {
-          var bookText = bookData[j].title.replace(pattern, '').toLowerCase().split(' ')
-              .concat(bookData[j].text.replace(pattern, '').toLowerCase().split(' '));
+          var bookText = bookData[j].title.replace(pattern, '')
+          		.toLowerCase().split(' ')
+              .concat(bookData[j].text.replace(pattern, '')
+              .toLowerCase().split(' '));
 
           for (var k = 0; k < bookText.length; k++) {
-            // if the data from each document has the key value push it to found    
+            // if the data from each document has the key value push it to found
             if (bookText.indexOf(noDups[i]) > -1) {
               found.push(j);
               // break the loop as the word exists in the document.
@@ -54,19 +57,20 @@ Index = function(filePath) {
         self.index[noDups[i]] = self.index[noDups[i]].concat(found);
     }
     // return the created index
-    console.log(self.index);
+    // console.log(self.index);
     return self.index;
 
   };
 
   this.getIndex = function() {
-      // returns an object that is an accurate index of the content of the JSON file.
+      // returns the index of the content of the JSON file.
       return this.index;
   };
 
   this.searchIndex = function(searchTerms) {
-      // returns an  object showing the indices for each word in the search terms parameter
-      var indexResult = {},  
+      // returns an  object showing the indices for each word in
+      // the search terms parameter
+      var indexResult = {},
           indexAsArray = Object.keys(this.index);
       var splitTerms;
       if (typeof searchTerms == 'string'){
@@ -75,16 +79,19 @@ Index = function(filePath) {
       else{
         splitTerms = searchTerms;
       }
-      // loop through array of search terms and check whether it exists in the main index
-      // if exist return index results
+      // loop through array of search terms and check whether it exists
+      // in the main index
+
       for(var i = 0; i < splitTerms.length; i++) {
-        if (indexAsArray.indexOf(splitTerms[i]) > -1) {
-          indexResult[splitTerms[i]] = this.index[splitTerms[i]];
-        }
+        if (indexAsArray.indexOf(splitTerms[i]) == -1) {
+        	indexResult[splitTerms[i]] = 'Error: the term couldn\'t be found';
+        	break;
+         }
+        indexResult[splitTerms[i]] = this.index[splitTerms[i]];
 
       }
       return indexResult;
-     
+
 
     };
 
@@ -97,33 +104,33 @@ var pattern = /[.',:]/gi;
 
   // for every object in the array get all words and remove punctuations
   for(var inc = 0; inc < data.length; inc++) {
-    
+
     doc = data[inc].title.replace(pattern, '').toLowerCase().split(' ')
       .concat(data[inc].text.replace(pattern, '').toLowerCase().split(' '));
-     
+
       noPunct = noPunct.concat(doc);
-    
+
   }
   // return the array of words sorted alphabetically
   return noPunct.sort();
 }
 function removeStopWords(data) {
-    stopWords = [
-        'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and',
-        'any', 'are', 'as', 'at', 'be', 'because', 'been', 'before', 'being',
-        'below', 'between', 'both', 'but', 'by', 'can', 'did', 'do', 'does',
-        'doing', 'don', 'down', 'during', 'each', 'few', 'for', 'from', 'further',
-        'had', 'has', 'have', 'having', 'he', 'her', 'here', 'hers', 'herself',
-        'him', 'himself', 'his', 'how', 'i', 'if', 'in', 'into', 'is', 'it', 'its',
-        'itself', 'just', 'me', 'more', 'most', 'my', 'myself', 'no', 'nor', 'not',
-        'now', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'our', 'ours',
-        'ourselves', 'out', 'over', 'own', 's', 'same', 'she', 'should', 'so', 'some',
-        'such', 't', 'than', 'that', 'the', 'their', 'theirs', 'them', 'themselves',
-        'then', 'there', 'these', 'they', 'this', 'those', 'through', 'to', 'too',
-        'under', 'until', 'up', 'very', 'was', 'we', 'were', 'what', 'when', 'where',
-        'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'you', 'your', 'yours',
-        'yourself', 'yourselves'
-    ];
+  var stopWords = [
+    'a', 'about', 'above', 'again', 'against', 'all', 'am', 'an', 'and',
+    'any', 'are', 'as', 'at', 'be', 'because', 'been', 'before', 'being',
+    'below', 'between', 'both', 'but', 'by', 'can', 'did', 'do', 'does',
+    'doing', 'don', 'down', 'during', 'each', 'few', 'for', 'from', 'further',
+    'had', 'has', 'have', 'having', 'he', 'her', 'here', 'hers', 'herself',
+    'him', 'himself', 'his', 'how', 'i', 'if', 'in', 'into', 'is', 'it', 'its',
+    'itself', 'just', 'me', 'more', 'most', 'my', 'myself', 'no', 'nor', 'not',
+    'now', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'our', 'ours',
+    'out', 'over', 'own', 's', 'same', 'she', 'should', 'so', 'some',
+    'such', 't', 'than', 'that', 'the', 'their', 'theirs', 'them',
+    'then', 'there', 'these', 'they', 'this', 'those', 'through', 'to', 'too',
+    'under', 'up', 'very', 'was', 'we', 'were', 'what', 'when', 'where',
+    'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'you', 'your',
+    'yourself', 'yourselves'
+  ];
     var noStopWords = [];
     for (var i = 0; i < data.length; i++) {
         if (stopWords.indexOf(data[i]) < 0) {
@@ -134,11 +141,12 @@ function removeStopWords(data) {
 }
 
 function removeDuplicates(data) {
-    noDuplicates = data;
+    var noDuplicates = data;
     // if two or more consecutive words are similar remove one of them
     for (var i = 0; i < noDuplicates.length; i++) {
-        if (noDuplicates[i] === noDuplicates[i + 1])
+        if (noDuplicates[i] === noDuplicates[i + 1]) {
             noDuplicates.splice(i, 1);
+          }
     }
     return noDuplicates;
 }
